80f36ec18c0c3ae6642a4995dbe3ed967fda3fcc
Max <rebelion5418@gmail.com>
2015-02-23 19:17:43 -0800

    Test for setting Exsistance

--- Glowing_Spice.py
+++ Glowing_Spice.py
@@ -6,16 +6,22 @@ name = "UNKNOWN_NAME"
 #print counter
 #wait = input("PRESS ENTER TO CONTINUE.")
 #firstTimeRunning()
+
 def testForConfig():
+    
     filename = "Glowing_Spice.py"
     path = os.path.realpath(__file__)
     cutpath = path[0:path.index(filename)]
-	
-
-
-
+    fullpath = cutpath + "settings.cfg"
+    return os.path.exists(fullpath)
 
 def firstTimeRunning():
     name = raw_input("Please enter your preferred user name: ")
-testForConfig()
-firstTimeRunning()
+
+if testForConfig() == True:
+    print "The file exists"
+    i = raw_input()
+else:
+    firstTimeRunning()
+
+
