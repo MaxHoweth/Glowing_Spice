8656419bcc4f1f3ac24530b175636f21c1718dfc
Max <rebelion5418@gmail.com>
2015-02-23 18:29:55 -0800

    Second Commit
    ---

--- Glowing_Spice.py
+++ Glowing_Spice.py
@@ -1,3 +1,15 @@
-counter = 100
-print counter
-wait = input("PRESS ENTER TO CONTINUE.")
+
+#name = "UNKNOWN_NAME"
+#counter = 100
+#print counter
+#wait = input("PRESS ENTER TO CONTINUE.")
+#firstTimeRunning()
+'''Function definition and invocation.'''
+
+def firstTimeRunning():
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Emily.")
+    print("Happy Birthday to you!")
+
+firstTimeRunning()
